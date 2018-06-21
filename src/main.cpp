9503df2131212e577af7a57a15acac147307ecf0
Josh Watson <josh@joshwatson.com>
2018-06-21 15:22:51 -0700

    Fixed indentation There was a mixing of tabs and spaces. Changed to just spaces.

--- src/main.cpp
+++ src/main.cpp
@@ -7,15 +7,15 @@ struct mach_o_x86;
 extern "C"
 {
     BINARYNINJAPLUGIN void CorePluginDependencies()
-	{
-		// Make sure we load after the original mach-o plugin loads
-		AddRequiredPluginDependency("platform_mac");
-	}
+    {
+        // Make sure we load after the original mach-o plugin loads
+        AddRequiredPluginDependency("platform_mac");
+    }
 
     BINARYNINJAPLUGIN bool CorePluginInit()
-	{
+    {
         BinaryViewType::Register(new FatMachOBinaryViewType<mach_o_x86_64>());
-		BinaryViewType::Register(new FatMachOBinaryViewType<mach_o_x86>());
-		return true;
+        BinaryViewType::Register(new FatMachOBinaryViewType<mach_o_x86>());
+        return true;
     }
 }
