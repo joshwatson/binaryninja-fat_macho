279de46888c908c0221fb76a44ab5d06a47297ff
Josh Watson <josh@joshwatson.com>
2019-04-24 13:12:21 -0700

    Update machoTypes.hpp
    Added cpusubtype to every structure

--- src/machoTypes.hpp
+++ src/machoTypes.hpp
@@ -10,15 +10,19 @@ using cpu_subtype_t = int;
 #define CPU_TYPE_X86_64         (CPU_TYPE_X86 | CPU_ARCH_ABI64)
 #define CPU_TYPE_ARM            ((cpu_type_t) 12)
 #define CPU_TYPE_ARM64          (CPU_TYPE_ARM | CPU_ARCH_ABI64)
+#define CPU_SUBTYPE_X86_ALL     ((cpu_subtype_t)3)
+#define CPU_SUBTYPE_X86_64_ALL  ((cpu_subtype_t)3)
 #define CPU_SUBTYPE_ARM64_ALL   ((cpu_subtype_t) 0)
 #define CPU_SUBTYPE_ARM_V7      ((cpu_subtype_t) 9)
 #define CPU_TYPE_POWERPC        ((cpu_type_t) 18)
+#define CPU_SUBTYPE_POWERPC_ALL ((cpu_subtype_t) 0)
 
 struct mach_o_x86_64
 {
     static const std::string name;
     static const std::string long_name;
     static const cpu_type_t cputype = CPU_TYPE_X86_64;
+    static const cpu_subtype_t cpusubtype = CPU_X86_64_ALL;
 };
 
 struct mach_o_x86
@@ -26,6 +30,7 @@ struct mach_o_x86
     static const std::string name;
     static const std::string long_name;
     static const cpu_type_t cputype = CPU_TYPE_X86;
+    static const cpu_type_t cpusubtype = CPU_X86_ALL;
 };
 
 struct mach_o_aarch64
@@ -49,4 +54,5 @@ struct mach_o_ppc
     static const std::string name;
     static const std::string long_name;
     static const cpu_type_t cputype = CPU_TYPE_POWERPC;
+    static const cpu_subtype_t cpusubtype = CPU_SUBTYPE_POWERPC_ALL;
 };
