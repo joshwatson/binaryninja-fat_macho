a1ada78f5104eef1171795e4c0c9879293f9099c
Josh Watson <josh@trailofbits.com>
2019-04-25 11:15:52 -0700

    Created .gitignore, cleaned up code and repo

--- src/FatMachOBinaryViewType.cpp
+++ src/FatMachOBinaryViewType.cpp
@@ -15,38 +15,18 @@ struct fat_arch
     uint32_t align;
 };
 
-template <typename ARCH>
-FatMachOBinaryViewType<ARCH>::FatMachOBinaryViewType() :
-    BinaryViewType(ARCH::name, ARCH::long_name) { }
-
-Ref<BinaryView> NewParentBinaryView(BinaryView* originalParent, DataBuffer& data)
+bool GetArchitectureInfo(BinaryView* data, fat_arch &arch, size_t i)
 {
-    // Ref<FileMetadata> file_metadata = new FileMetadata();
-    BNBinaryView* view = BNCreateBinaryDataViewFromBuffer(originalParent->GetFile()->GetObject(), data.GetBufferObject());
-    LogDebug("view = %p", view);
-    return new BinaryView(view);
-}
-
-template <typename ARCH>
-BinaryView* FatMachOBinaryViewType<ARCH>::Create(BinaryView* data)
-{
-    LogInfo("BVT Create");
-    LogDebug("Read header");
-    // Look for a fat_arch structure that matches our type
-    fat_header header;
-    data->Read(&header, 0, sizeof(header));
-
-    header.magic = __builtin_bswap32(header.magic);
-    header.nfat_arch = __builtin_bswap32(header.nfat_arch);
-
-    LogDebug("header.magic = %016x", header.magic);
-    LogDebug("arch.nfat_arch = %16x", header.nfat_arch);
-    fat_arch arch;
-    for (unsigned long i = 0; i < header.nfat_arch; i++)
-    {
         LogDebug("Reading arch %d", i);
-        data->Read(&arch, sizeof(header) + sizeof(arch) * i, sizeof(arch));
-        
+        if (data->Read(
+                &arch,
+                sizeof(fat_header) + sizeof(arch) * i,
+                sizeof(arch)
+            ) != sizeof(arch))
+        {
+            return false;
+        }
+
         // fix the endianness. This looks so dirty
         arch.size = __builtin_bswap32(arch.size);
         arch.offset = __builtin_bswap32(arch.offset);
@@ -56,13 +36,57 @@ BinaryView* FatMachOBinaryViewType<ARCH>::Create(BinaryView* data)
 
         LogDebug("arch.cputype %08x", arch.cputype);
 
-        if (arch.cputype == ARCH::cputype && arch.cpusubtype == ARCH::cpusubtype)
+        return true;
+}
+
+template <typename ARCH>
+FatMachOBinaryViewType<ARCH>::FatMachOBinaryViewType() :
+    BinaryViewType(ARCH::name, ARCH::long_name) {}
+
+Ref<BinaryView> NewParentBinaryView(
+    BinaryView *originalParent,
+    DataBuffer &data
+)
+{
+    BNBinaryView *view = BNCreateBinaryDataViewFromBuffer(
+        originalParent->GetFile()->GetObject(),
+        data.GetBufferObject()
+    );
+    LogDebug("view = %p", view);
+    return new BinaryView(view);
+}
+
+template <typename ARCH>
+BinaryView *FatMachOBinaryViewType<ARCH>::Create(BinaryView *data)
+{
+    LogInfo("BVT Create");
+    LogDebug("Read header");
+
+    // Look for a fat_arch structure that matches our type
+    fat_header header;
+    data->Read(&header, 0, sizeof(header));
+
+    header.magic = __builtin_bswap32(header.magic);
+    header.nfat_arch = __builtin_bswap32(header.nfat_arch);
+
+    LogDebug("header.magic = %08x", header.magic);
+    LogDebug("arch.nfat_arch = %08x", header.nfat_arch);
+    fat_arch arch;
+    for (unsigned long i = 0; i < header.nfat_arch; i++)
+    {
+        if (!GetArchitectureInfo(data, arch, i))
+        {
+            LogDebug("Failed to get architecture information");
+            break;
+        }
+
+        if (arch.cputype == ARCH::cputype)
         {
             break;
         }
     }
 
-    if (arch.cputype != ARCH::cputype || arch.cpusubtype != ARCH::cpusubtype)
+    if (arch.cputype != ARCH::cputype)
     {
         LogDebug("Couldn't find arch");
         // couldn't find it
@@ -73,14 +97,18 @@ BinaryView* FatMachOBinaryViewType<ARCH>::Create(BinaryView* data)
     length += arch.align - (length % arch.align);
     DataBuffer fat_data = data->ReadBuffer(arch.offset, length);
     auto parent = NewParentBinaryView(data, fat_data);
-    BNBinaryView* child = BNCreateBinaryViewOfType(BinaryViewType::GetByName("Mach-O")->GetObject(), parent->GetObject());
+    BNBinaryView *child = BNCreateBinaryViewOfType(
+        BinaryViewType::GetByName("Mach-O")->GetObject(),
+        parent->GetObject()
+    );
     return new BinaryView(child);
 }
 
 template <typename ARCH>
-bool FatMachOBinaryViewType<ARCH>::IsTypeValidForData(BinaryView* data)
+bool FatMachOBinaryViewType<ARCH>::IsTypeValidForData(BinaryView *data)
 {
     LogDebug("Read header");
+
     // Look for a fat_arch structure that matches our type
     fat_header header;
     data->Read(&header, 0, sizeof(header));
@@ -98,25 +126,19 @@ bool FatMachOBinaryViewType<ARCH>::IsTypeValidForData(BinaryView* data)
     fat_arch arch;
     for (unsigned long i = 0; i < header.nfat_arch; i++)
     {
-        LogDebug("Reading arch %d", i);
-        data->Read(&arch, sizeof(header) + sizeof(arch) * i, sizeof(arch));
-        
-        // fix the endianness. This looks so dirty
-        arch.size = __builtin_bswap32(arch.size);
-        arch.offset = __builtin_bswap32(arch.offset);
-        arch.cputype = __builtin_bswap32(arch.cputype);
-        arch.cpusubtype = __builtin_bswap32(arch.cpusubtype);
-        arch.align = __builtin_bswap32(arch.align);
+        if (!GetArchitectureInfo(data, arch, i))
+        {
+            LogDebug("Failed to get architecture information");
+            break;
+        }
 
-        LogDebug("arch.cputype %08x", arch.cputype);
-
-        if (arch.cputype == ARCH::cputype && arch.cpusubtype == ARCH::cpusubtype)
+        if (arch.cputype == ARCH::cputype)
         {
             break;
         }
     }
 
-    return (arch.cputype == ARCH::cputype && arch.cpusubtype == ARCH::cpusubtype);
+    return (arch.cputype == ARCH::cputype);
 }
 
 template class FatMachOBinaryViewType<mach_o_x86_64>;
